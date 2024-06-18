8b2b333d3b4546ac1222c46e0a5db9edfd6b746d
Nicolas Guilherme Matheus <nicolas.gmop@gmail.com>
2024-06-17 23:00:41 -0300

    feat(app-routing): configurando rotas + rota mfe externo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
+  { path: 'pokemon-list', loadChildren: () => import('pokemonMfeList/Module').then(m => m.RemoteEntryModule) },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
